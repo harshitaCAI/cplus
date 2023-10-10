e8ff625cda773c38be588a0935e2ba6ceb05cf04
harshita <harshitacelebai@gmail.com>
2023-10-10 13:58:59 +0530

    added case 4

--- example.cpp
+++ example.cpp
@@ -6,7 +6,7 @@ int main()
       int a,b;
 cin>>a>>b;
       int choice;
-     cout<<"\nchoose 1 to add , 2 to subtract and 3 to multuply";
+     cout<<"\nchoose 1 to add , 2 to subtract and 3 to multuply and 4 to divide";
       cin>>choice;
       int ans;
       switch(choice)
@@ -14,6 +14,7 @@ cin>>a>>b;
        	     case 1: ans=a+b; break;
                case 2: ans=a-b; break;
        	case 3: ans=a*b; break;
+	case 4: ans=a/b;break;
       }
 cout<<ans;
 cout<<"Code run successfully";
